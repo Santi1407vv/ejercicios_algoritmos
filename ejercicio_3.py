9b0f9e361f347b984c14107c9b993769fb5a44a9
Nicolas <nicolastobon2424gmail.com>
2023-08-01 11:32:49 -0500

    Implementacion ejercicio 3

---
+++ ejercicio_3.py
@@ -0,0 +1,16 @@
+import random
+
+def generar_lista_aleatoria(longitud, minimo, maximo):
+    lista_aleatoria = [random.randint(minimo, maximo) for _ in range(longitud)]
+    return lista_aleatoria
+
+
+try:
+    longitud_lista = 10
+    valor_minimo = 1
+    valor_maximo = 100
+
+    lista_aleatoria = generar_lista_aleatoria(longitud_lista, valor_minimo, valor_maximo)
+    print("Lista de números aleatorios:", lista_aleatoria)
+except ValueError as ve:
+    print(ve)
