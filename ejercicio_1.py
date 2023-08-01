64987acf63fd6ab00a8a9279a3deaa23e96e8764
Nicolas <nicolastobon2424gmail.com>
2023-08-01 11:32:19 -0500

    Implementacion ejercicio 1

---
+++ ejercicio_1.py
@@ -0,0 +1,11 @@
+def main():
+    nombre = input("Por favor, ingresa tu nombre: ")
+
+    edad = input("ingresa tu edad: ")
+
+    print("Nombre: " + nombre)
+    print("Edad: " + edad)
+
+
+if __name__ == "__main__":
+    main()
