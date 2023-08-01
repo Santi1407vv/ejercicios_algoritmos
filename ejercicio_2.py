236b9a67a561570260cf477819e0cf0d8aa7cc3e
Nicolas <nicolastobon2424gmail.com>
2023-08-01 11:32:32 -0500

    Implementacion ejercicio 2

---
+++ ejercicio_2.py
@@ -0,0 +1,12 @@
+def calcular_area_circulo(radio):
+    if radio < 0:
+        raise ValueError("El radio no puede ser negativo.")
+    area = 3.14159 * radio * radio
+    return area
+
+try:
+    radio_ingresado = float(input("Ingrese el radio del círculo: "))
+    area_calculada = calcular_area_circulo(radio_ingresado)
+    print("El área del círculo con radio {} es: {:.2f}".format(radio_ingresado, area_calculada))
+except ValueError as ve:
+    print(ve)
